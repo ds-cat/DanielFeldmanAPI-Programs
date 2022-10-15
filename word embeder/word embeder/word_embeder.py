9903813d20d1d5adef8038646217901ee05859ff
ds-cat <danielef@umich.edu>
2022-10-15 14:58:40 -0400

    Word embeder fix
    Word embedder works better now and embeds whole sentences. TODO Need to convert embeded sentences into clusters

--- word embeder/word embeder/word_embeder.py
+++ word embeder/word embeder/word_embeder.py
@@ -1,22 +1,41 @@
-from gensim.test.utils import common_texts
 from gensim.models import Word2Vec
+
+import nltk
+from nltk.cluster import KMeansClusterer
+
+import numpy as np 
+import csv
+
+from sent2vec.vectorizer import Vectorizer
+  
+from sklearn import cluster
+from sklearn import metrics
+  
 # Sentences for which you want to create embeddings,
 # passed as an array in embed()
 inputFile = open("reviews.txt", mode="r", encoding="utf-8")
-Sentences = []
+sentences = []
 for line in inputFile:
-    lineStrip = line.strip()
-    line_list = lineStrip.split()
-    Sentences.append(line_list)
+    
+    sentences.append(line[:512])#sentance 2 vec can only handle sentance of 512 characters or less
+
+
 inputFile.close()
-#print(Sentences)
-embeddings = Word2Vec(Sentences)
-  
-# Printing embeddings of each sentence
-print(embeddings)
-  
-# To print each embeddings along with its corresponding 
-# sentence below code can be used.
-#for i in range(len(Sentences)):
-  #  print(Sentences[i])
-  #  print(embeddings[i]
+saved = True
+
+#WARNING THIS WILL TAKE A LONG TIME 
+vecMod = (sentences)
+vectorizer = Vectorizer()
+vectorizer.run(sentences)
+vectors = vectorizer.vectors
+#print(vectors)
+
+#save voctorized sentacnes as cvs
+with open('vectors_save', 'w') as f:
+    # using csv.writer method from CSV package
+    write = csv.writer(f)
+    write.writerows(vectors)
+
+    #TODO: turn vectored sentaces into X,Y vectors
+
+    
